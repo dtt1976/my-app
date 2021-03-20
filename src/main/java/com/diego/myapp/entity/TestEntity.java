5c5188e32bf34dfeb8e798dffac887732f6ef208
dtt1976 <diegotorrestourn@gmail.com>
2021-03-19 22:49:53 -0300

    Nombre del proyecto:my-app. Descripcion version final de proyecto my-app

---
+++ src/main/java/com/diego/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.diego.myapp.entity;
+
+/**
+ * @author dtt
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para compilacion del archivo testentity
+	 */
+		
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
